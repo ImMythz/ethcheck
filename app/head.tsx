5cd2724814d315a5beeb62f7b088ebc66f76e1f2
Nate Worley <immythz@gmail.com>
2025-04-16 23:52:15 -0500

    added favicons

--- app/head.tsx
+++ app/head.tsx
@@ -2,6 +2,15 @@ export default function Head() {
   return (
     <>
       <meta name="google-adsense-account" content="ca-pub-9484338643096775" />
+      
+      {/* Favicon / App Icons */}
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+      <link rel="icon" type="image/png" sizes="192x192" href="/web-app-manifest-192x192.png" />
+      <link rel="icon" type="image/png" sizes="512x512" href="/web-app-manifest-512x512.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <meta name="theme-color" content="#000000" />
     </>
   )
 }
